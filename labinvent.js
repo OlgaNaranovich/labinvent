2a6c9c5abc66173b8dc397b32bd21a61cad71397
OlgaNaranovich <naranovicholga@gmail.com>
2019-01-25 12:28:59 +0300

    refactor: style refactoring

--- labinvent.js
+++ labinvent.js
@@ -1,11 +1,33 @@
 function toggleMenu() {
-	var toggleMnu = document.getElementById("toggle-menu");
-
-	if (!toggleMnu.classList.contains("on")) {
-		toggleMnu.classList.add("on");
+	const toggleMnu = document.getElementById('toggle-menu');
+	const folderTree = document.querySelector('.aside__folderTree');
+	if (!toggleMnu.classList.contains('on')) {
+		toggleMnu.classList.add('on');
 	} else
-		toggleMnu.classList.remove("on");
+		toggleMnu.classList.remove('on');
+		
+	if (!folderTree.classList.contains('show-folderTree')) {
+		folderTree.classList.add('show-folderTree');
+		console.log(folderTree);
+	} else {
+		folderTree.classList.remove('show-folderTree');
+	}
 }
+
+const showWidgets = document.querySelector('.show-widgets');
+showWidgets.addEventListener('click', function() {
+	const widgets = document.querySelector('.widgets');
+	const main = document.querySelector('.main');
+	console.log(widgets);
+	console.log(main);
+	if (!widgets.classList.contains('hidden') ) {
+		widgets.classList.add('hidden');
+		main.classList.add('hidden');
+	} else {
+		widgets.classList.remove('hidden');
+		main.classList.remove('hidden');
+	}
+});
 
 $(document).ready(function() {
   $('select').niceSelect();
